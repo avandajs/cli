a7f64936cab5518474591d26cb268d72418408cf
Wharley01 <hackerslord96@gmail.com>
2021-09-29 15:54:39 +0100

    Added middleware code template generation file

---
+++ defaults/tmps/middleware.tmp.ts
@@ -0,0 +1,20 @@
+import Template from "./Template";
+
+let middleware: Template = function (assetName: string,meta: object){
+
+    return `import {Middleware} from "avanda-http/middleware";
+import {Request, Response} from "avanda-http";
+
+export default class ${assetName} implements Middleware{
+    validate(res: Response, req: Request): boolean {
+        return false;
+    }
+    onFailure(res: Response, req: Request): Response {
+        // fall back response
+        return res.success('This error message is showing because validate method returns false');
+    }
+
+}`
+}
+
+export default middleware
