b87774e17d85f7bb6416bb2faab1c2d93c7984fe
Wharley01 <hackerslord96@gmail.com>
2021-09-29 15:54:23 +0100

    Added controller generation template file

---
+++ defaults/tmps/controller.ts
@@ -0,0 +1,28 @@
+import Template from "./Template";
+
+let controller: Template = function (assetName: string, meta: {modelName?: string}){
+
+    const {modelName} = meta;
+
+    return `import {Controller, Request, Response} from "avanda-http/index";
+import {get} from "avanda-http/verbs";
+${modelName ? `import Model from "../models/${modelName}"\n`:''}
+export default class ${assetName} extends Controller {
+    ${modelName ? 'model?: Model':''}
+    
+    @get()
+    async get(res: Response,req: Request){
+
+        let users = ${modelName ? `await this.model
+            ?.where('first_name')
+            .like('%aisha')
+            .orderBy('id','DESC')
+            .all();` : 'null;'}
+        
+        return res.success<any>('hello world',users)
+    }
+}
+`
+}
+
+export default controller
