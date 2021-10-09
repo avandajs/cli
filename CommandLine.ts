d23a44d30f3a0d1c1db3ad8caf636754e62325ae
Wharley01 <hackerslord96@gmail.com>
2021-10-09 02:11:42 +0100

    make connection and models availble to commandline interfaces

--- CommandLine.ts
+++ CommandLine.ts
@@ -1,7 +1,11 @@
 import {Command} from "commander";
+import {Sequelize} from "sequelize";
+import {Model} from "@avanda/orm"
 
 export default interface CommandLine {
     command: string
+    models?: {[k: string]: new (connection: Sequelize) => Model}
+    connection?: Sequelize
     options?: {
         option: string,
         description: string
